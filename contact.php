d81ca9b6685d50d3dd93aadba057f1b1d79ad079
hospitalityminds <61229482+hospitalityminds@users.noreply.github.com>
2021-12-16 12:38:08 +0530

    EDIT

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
 		$headers="From: ".$email;
 
 		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+			echo "<h1>Sent Successfully! Thank you"." ".$firstname.", We will contact you shortly!</h1>";
 		}
 		else{
 			echo "Something went wrong!";
