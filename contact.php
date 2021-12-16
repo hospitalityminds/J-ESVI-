f475762b4181df318f59a375e48dea064e7aef0a
hospitalityminds <61229482+hospitalityminds@users.noreply.github.com>
2021-12-16 12:25:43 +0530

    edit

--- contact.php
+++ contact.php
@@ -1,13 +1,15 @@
 <?php
 	if(isset($_POST['submit'])){
-		$name=$_POST['name'];
+		$name=$_POST['firstname'];
+  	$name=$_POST['lastname'];
 		$email=$_POST['email'];
-		$phone=$_POST['phone'];
-		$msg=$_POST['msg'];
+  	$msg=$_POST['message'];
+		// $phone=$_POST['phone'];
 
-		$to='reservations@hoteljesvi.com'; // Receiver Email ID, Replace with your email ID
+
+		$to='shaizathirst@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Form Submission';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$message="First Name :".$firstname."\n"."last Name :".$lastname."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
 
 		if(mail($to, $subject, $message, $headers)){
